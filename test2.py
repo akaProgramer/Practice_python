7e57ada72db4af60a729e80ad8f57da658230ff0
akaProgramer <51360466+akaProgramer@users.noreply.github.com>
2020-07-27 09:38:10 +0530

    rock paper and seccisors

--- test2.py
+++ test2.py
@@ -1,7 +1,6 @@
 from threading import Timer
-timeout = 4
+timeout = 6
 t = Timer(timeout, print, ['Sorry, times up'])
-print(t)
 t.start()
 prompt = input("You have %d seconds to choose the correct answer...\n" % timeout)
 t.cancel()
